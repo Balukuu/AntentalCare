9444f067cce552f66317af87e0c1bb1f5f9606ba
Balukuu <edgarbaluku@gmail.com>
2021-09-02 12:52:09 +0300

    Added A SplashActivity

---
+++ app/src/main/java/com/example/antentalcare/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.antentalcare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
