0430ae4290f96f88226850e6f8410cb2fd609a04
Balukuu <edgarbaluku@gmail.com>
2021-09-05 15:25:38 +0300

    Changes

--- app/src/main/java/com/example/antentalcare/SplashActivity.java
+++ app/src/main/java/com/example/antentalcare/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this,SignUp.class);
                 startActivity(intent);
                 finish();
             }
