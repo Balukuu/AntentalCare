45e21f097a611ba056890b36dc9bea76cba663dc
Balukuu <edgarbaluku@gmail.com>
2021-09-05 15:19:33 +0300

    LogInchange

---
+++ app/src/main/java/com/example/antentalcare/Doc_fragment.java
@@ -0,0 +1,24 @@
+package com.example.antentalcare;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Doc_fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.doc_fragment, container, false);
+
+        return view;
+
+    }
+}
