10be2403071b56ce2dd0fe353aa1b4172ce25352
Balukuu <edgarbaluku@gmail.com>
2021-09-05 15:51:12 +0300

    Changes

--- app/src/main/java/com/example/antentalcare/MainActivity.java
+++ app/src/main/java/com/example/antentalcare/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.antentalcare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_main);
+
+
+
     }
 }
